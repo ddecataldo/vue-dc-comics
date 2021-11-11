44bd43aaab2d5db3f4918a95d04da2c6a923fb9c
ddecataldo <davidedecataldo92@gmail.com>
2021-11-11 16:27:05 +0100

    Upload components: Header e Main quasi completi

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="container">
+        <div class="logo">
+            <img src="../assets/dc-logo.png" />
+        </div>
+        <div class="menu">
+            <ul>
+                <li 
+                v-for="(item, i) in navbarItems" 
+                :key="i">
+                    <a :href="item.href">{{ item.title }}</a>
+                </li>
+            </ul>
+        </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data(){
+        return {
+            navbarItems: [
+                {
+                    title: "Characters",
+                    href: "#",
+                },
+                {
+                    title: "Comics",
+                    href: "#",
+                },
+                {
+                    title: "Movies",
+                    href: "#",
+                },
+                {
+                    title: "TV",
+                    href: "#",
+                },
+                {
+                    title: "Games",
+                    href: "#",
+                },
+                {
+                    title: "Collectibles",
+                    href: "#",
+                },
+                {
+                    title: "Videos",
+                    href: "#",
+                },
+                {
+                    title: "Fans",
+                    href: "#",
+                },
+                {
+                    title: "News",
+                    href: "#",
+                },
+                {
+                    title: "Shop",
+                    href: "#",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <div class="container">
+            <h1> --> Content goes Here  </h1>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
