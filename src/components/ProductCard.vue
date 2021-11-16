44fe11ef4eeaf907691010c89c532e284ac86736
ddecataldo <davidedecataldo92@gmail.com>
2021-11-16 09:31:36 +0100

    Card generate con l'utilizzo dei props.

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="product-card">
+
+    <img :src="thumb" alt="" />
+
+    <h4>{{ series }}</h4>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    thumb: String,
+    price: {
+      type: String,
+      required: true,
+    },
+    series: String,
+    type: String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
